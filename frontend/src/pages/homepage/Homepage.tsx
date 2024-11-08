7685427e6e23ec65df80bf6507869b3bb733fe39
Lígia Abreu <lilly.abreu20@gmail.com>
2024-11-07 21:16:11 -0300

    Revert "fix: overflow homepage"
    This reverts commit a96a36beb20fc4053f358426f91448c7a43c57a2.

--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -3,7 +3,7 @@ import Helmet from "../../assets/icons/Helmet";
 import SpeedDialComponent from "../../components/speed-dial/SpeedDial";
 import "./Homepage.css";
 import { useNavigate } from "react-router-dom";
-import RoomsList from "./roomsList";
+
 const Homepage = () => {
 	const rooms = [
 		{
@@ -11,31 +11,31 @@ const Homepage = () => {
 			status: "waiting",
 			playersOn: 2,
 			totalPlayers: 4,
-			laps: 4,
+			labs: 4,
 		},
 		{
 			id: "dad23gg2",
 			status: "racing",
 			playersOn: 2,
 			totalPlayers: 4,
-			laps: 4,
+			labs: 4,
 		},
 		{
 			id: "dad23gg2",
 			status: "waiting",
 			playersOn: 2,
 			totalPlayers: 4,
-			laps: 4,
+			labs: 4,
 		},
 		{
 			id: "dad23gg2",
 			status: "racing",
 			playersOn: 2,
 			totalPlayers: 4,
-			laps: 4,
+			labs: 4,
 		},
 	];
-	const navigate = useNavigate();
+	const navigate = useNavigate()
 	return (
 		<section id="homepage">
 			<section id="content-homepage">
@@ -44,24 +44,44 @@ const Homepage = () => {
 					<h2>It’s Racing Time!</h2>
 					<p>Grab your helmet and fast your seatbelt!</p>
 				</div>
-				<div id="body-room-list">
-					<div id="room-list">
-						<div id="room-list-head">
-							<h3>Id</h3>
-							<h3>Status</h3>
-							<h3>Players</h3>
-							<h3>Laps</h3>
-							<h3 id="blank"></h3>
-						</div>
-						<div id="room-list-body">
+				<div id="content-table">
+					<table>
+						<thead>
+							<tr>
+								<th>id</th>
+								<th>status</th>
+								<th>players</th>
+								<th>labs</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
 							{rooms.map((room, index) => (
-								<RoomsList key={index} room={room} index={index}/> 
+								<tr key={index}>
+									<td>{room.id}</td>
+									{room.status === "waiting" ? (
+										<td className="waiting">
+											{room.status}
+										</td>
+									) : (
+										<td>{room.status}</td>
+									)}
+									<td>{`${room.playersOn}/${room.totalPlayers}`}</td>
+									<td>{room.labs}</td>
+									{room.status === "waiting" ? (
+										<td>
+											<button>join</button>
+										</td>
+									) : (
+										<td>
+											<Helmet />
+										</td>
+									)}
+								</tr>
 							))}
-						</div>
-					</div>
-					<button onClick={() => navigate("/lobby")}>
-						Create Race
-					</button>
+						</tbody>
+					</table>
+					<button onClick={() => navigate('/lobby')}>Create Race</button>
 				</div>
 			</section>
 		</section>
