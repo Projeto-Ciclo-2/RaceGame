2d10b8ab076b8406d51199fd84449b3a99a6a62d
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-11-18 09:55:28 -0300

    feat: new layout for the game page end screen

--- frontend/src/pages/end/EndScreen.tsx
+++ frontend/src/pages/end/EndScreen.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { IPlayer } from "../../interfaces/IRoom";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { src } from "../../assets/enum/enumSrc";
 import "./endScreen.css";
 import { sortPlayers } from "../game/tools/sortPlayers";
 import { useRoom } from "../../context/RoomContext";
 import { calcPercentage } from "./calcPercentage";
+import BxsHome from "../../components/icons/home";
 
 export default function EndScreen(props: {
 	alive: boolean;
@@ -33,45 +34,70 @@ export default function EndScreen(props: {
 		RoomsContext.reset();
 	}
 
+	const navigate = useNavigate();
+
 	if (!props.alive) {
 		return <></>;
 	}
+
+	function navigateToPageHome() {
+		navigate("/home");
+	}
+
 	return (
-		<div id="container">
-			<div id="endScreen">
-				<p>
-					{props.winner === props.me.username
-						? "Victory"
-						: "Game Over"}
-				</p>
-				<img src={src.wheel} alt="wheel" />
-				<Link id="endScreenBTN" to={"/home"} onClick={clearThisRoom}>
-					Voltar ao início
-				</Link>
+		<div id="end-game">
+			<div id="back-home" onClick={navigateToPageHome}>
+				<BxsHome />
 			</div>
-			<div id="endScreenTable">
-				{sortedPlayers.map((p, i) => {
-					return (
-						<section
-							key={p.id}
-							className={props.me?.id === p.id ? "isMe" : ""}
-						>
-							<p>
-								<span>{i + 1}º</span>
-								<span>{p.username}</span>
-							</p>
-							<span>
-								{p.done_laps} voltas{" "}
-								{calcPercentage(
-									p.checkpoint,
-									p.done_laps,
-									props.laps || 3
-								).toFixed(0)}
-								% concluído
-							</span>
-						</section>
-					);
-				})}
+			<div id="container">
+				<section id="game-result">
+					<div id="result">
+						{props.winner === props.me.username ? (
+							<p style={{ color: "#00ACD6" }}>winner</p>
+						) : (
+							<p style={{ color: "#FE3704" }}>you lost</p>
+						)}
+					</div>
+					<div
+						id="img-result"
+						style={{
+							backgroundImage:
+								props.winner === props.me.username
+									? "url('./assets/imgs/img-winner-rac.webp')"
+									: "url('./assets/imgs/img-lost-rac.webp')",
+						}}
+					></div>
+				</section>
+				<section id="race">
+					<div id="race-result">
+						<p>race result</p>
+						<div>
+							<p>laps</p>
+							<p>completed</p>
+						</div>
+					</div>
+					<ul>
+						{sortedPlayers.map((p, i) => (
+							<li key={i}>
+								<div id="div1">
+									<p className="position-number">{i + 1}</p>
+									<p className="username">{p.username}</p>
+								</div>
+								<div>
+									<p className="laps-number">{p.done_laps}</p>
+									<p className="completed">
+										{calcPercentage(
+											p.checkpoint,
+											p.done_laps,
+											props.laps || 3
+										).toFixed(0)}
+										%
+									</p>
+								</div>
+							</li>
+						))}
+					</ul>
+				</section>
 			</div>
 		</div>
 	);
